ea6e485e86e9e7a479a71616a083f2b661068ea1
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2023-11-30 12:13:21 +0600

    verify token middleware created

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const app = express()
-app.use(express.json());
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-app.use(cors())
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const port = process.env.PORT || 5000
-app.get('/', async (req, res) => {
-  res.send('pet adoption is running')
-})
 
-const { useParams } = require('react-router-dom')
+// built middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+//custom middlewares
+const verifyToken = async (req, res, next) => {
+
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: 'access unauthorized ' })
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: 'bad request' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vqva6ft.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,6 +40,11 @@ const client = new MongoClient(uri, {
   }
 });
 
+app.get('/', async (req, res) => {
+  res.send('pet adoption is running')
+})
+
+
 async function run() {
   try {
     const petCollection = client.db('petsDB').collection('pets')
@@ -30,16 +52,17 @@ async function run() {
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    // pets function
     //jwt generation
     app.post('/jwt', async (req, res) => {
       const user = req.body
       console.log('user email', user)
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' })
       console.log('the token is ', token)
-      res.send(token)
+      res.send({ token })
     })
-    app.get('/pets', async (req, res) => {
+    // pets function
+
+    app.get('/pets', verifyToken, async (req, res) => {
       const result = await petCollection.find().toArray()
       res.send(result)
     })
